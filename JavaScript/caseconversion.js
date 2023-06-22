338e330750ebf04f33c76b9093dc47328aad1233
Yasin N K <yasinnk15@gmail.com>
2023-06-22 22:50:24 +0530

    Update caseconversion.js

--- JavaScript/caseconversion.js
+++ JavaScript/caseconversion.js
@@ -1,4 +1,4 @@
 var name = prompt("Enter your name");
 var first = (name.slice(0, 1)).toUpperCase();
 var rest = (name.slice(1, name.length)).toLowerCase();
-console.log("Your name is " + first + rest);
+console.log("Your name is " + first + rest)
